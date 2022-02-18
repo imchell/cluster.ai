8d5a0873e2a1188c69ad7eed580788ca30ff3b91
Chell <im.chell@outlook.com>
2022-02-18 21:00:44 +0800

    feat: implement gallery

--- pages/components/Gallery.tsx
+++ pages/components/Gallery.tsx
@@ -1,11 +1,24 @@
-import { Grid } from '@mantine/core';
+import { Grid, Image, Text } from '@mantine/core';
+import { useRef } from 'react';
 
 export function Gallery(props: { files: File[] }) {
   return (
     <Grid>
       {props.files.map((file) => (
-        <Grid.Col span={2}>1</Grid.Col>
+        <Grid.Col span={2}>
+          <Image src={URL.createObjectURL(file)} />
+        </Grid.Col>
       ))}
     </Grid>
   );
+}
+
+function previewFiles(files: File[]) {
+  let urls: (string | ArrayBuffer | null)[] = [];
+  files.forEach((file) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    urls.concat(reader.result);
+  });
+  return urls;
 }
