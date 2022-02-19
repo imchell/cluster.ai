22e95480ba4b11a050ef45a7720114dfa989760c
Chell <im.chell@outlook.com>
2022-02-19 23:56:29 +0800

    feat: model

--- pages/components/PageSwitcher.tsx
+++ pages/components/PageSwitcher.tsx
@@ -1,9 +1,15 @@
 import { Navbar, Text, Button, Container, Space } from '@mantine/core';
+import { classification } from '../model/net';
+import { fileURLsAtom, typesAtom } from '../store/data';
+import { useAtom } from 'jotai';
 
 export function PageSwitcher() {
+  const [fileURLs, setFileURLs] = useAtom(fileURLsAtom);
+  const [types, setTypes] = useAtom(typesAtom);
+
   return (
     <>
-      <Button>Train</Button>
+      <Button onClick={() => classification(fileURLs, types)}>Train</Button>
       <Space h="md" />
       <Button>Predict</Button>
     </>
--- pages/model/net.js
+++ pages/model/net.js
@@ -1,16 +1,44 @@
-const mobilenet = require('@tensorflow-models/mobilenet');
+const tf = require('@tensorflow/tfjs');
+const mobilenetModule = require('@tensorflow-models/mobilenet');
+const knnClassifier = require('@tensorflow-models/knn-classifier');
 
-async function app() {
-  const img = document.getElementById('img');
+export const classification = async function (fileURLs, types) {
+  let imgs = generateImgs(fileURLs);
 
-  // Load the model.
-  const model = await mobilenet.load();
+  const classifier = knnClassifier.create();
+  const mobilenet = await mobilenetModule.load();
 
-  // Classify the image.
-  const predictions = await model.classify(img);
+  console.log('start');
+  console.log(imgs);
 
-  console.log('Predictions: ');
-  console.log(predictions);
-}
+  let undecidedImgs = [];
 
-app();
+  for (const i = 0; i < imgs.length; i++) {
+    if (types[i] === 'undecided') {
+      undecidedImgs.push(imgs[i]);
+    } else {
+      const logits = mobilenet.infer(imgs[i], true);
+      classifier.addExample(logits, types[i]);
+    }
+  }
+
+  console.log('success');
+  console.log('predict');
+
+  for (const i = 0; i < undecidedImgs.length; i++) {
+    const x = tf.browser.fromPixels(undecidedImgs[i]);
+    const xlogits = mobilenet.infer(x, true);
+    console.log('Predictions:');
+    console.log(classifier.predictClass(xlogits));
+  }
+};
+
+const generateImgs = function (fileURLs) {
+  let imgs = [];
+  for (const fileURL of fileURLs) {
+    const img = document.createElement('img');
+    img.setAttribute('src', fileURL);
+    imgs.push(img);
+  }
+  return imgs;
+};
