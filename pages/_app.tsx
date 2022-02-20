af42029c84082de22db580e00449645d5ed81248
Chell <im.chell@outlook.com>
2022-02-20 23:07:05 +0800

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider, NormalizeCSS, GlobalStyles } from '@mantine/core';
+import { MantineProvider, NormalizeCSS, GlobalStyles, useMantineTheme } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>cluster.ai</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
@@ -17,6 +16,13 @@ export default function App(props: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         ></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" />
+        <meta name="msapplication-TileColor" />
+        <meta name="theme-color" />
       </Head>
 
       <MantineProvider
