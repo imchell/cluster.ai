2fe99cfc67933383126177cca44ea07e2f6417ea
Chell <im.chell@outlook.com>
2022-02-20 17:28:32 +0800

    feat: guide style

--- pages/components/Guide.tsx
+++ pages/components/Guide.tsx
@@ -1,17 +1,25 @@
-import { Card, Text, useMantineTheme } from '@mantine/core';
+import { Card, Text, useMantineTheme, Center, Image, List, Space, Paper } from '@mantine/core';
 
 export function Guide() {
   const theme = useMantineTheme();
 
   return (
-    <Card
-      style={{
-        backgroundColor: theme.colors.operations[0],
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <Text>Drop File Here.</Text>
-    </Card>
+    <Center style={{ width: '100%', height: '100%' }}>
+      <Paper style={{ backgroundColor: theme.colors.operations[0] }}>
+        <Center>
+          <Image src="/logo/album.svg" style={{ width: '300px', height: 'auto' }} />
+        </Center>
+        <Center>
+          <Text weight={700}>
+            <List type="order" style={{ color: theme.colors.operations[3] }}>
+              <List.Item>drop your images here</List.Item>
+              <List.Item>tag images and leave some untagged</List.Item>
+              <List.Item>train and wait</List.Item>
+              <List.Item>click predict to get results for those untagged</List.Item>
+            </List>
+          </Text>
+        </Center>
+      </Paper>
+    </Center>
   );
 }
